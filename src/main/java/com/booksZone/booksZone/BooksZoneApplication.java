b1f277427e4f02fe71d55142ec5cb74935f0fa37
Emediong <emediongcharlie@gmail.com>
2025-06-06 09:09:50 +0100

    Week One Task

---
+++ src/main/java/com/booksZone/booksZone/BooksZoneApplication.java
@@ -0,0 +1,13 @@
+package com.booksZone.booksZone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksZoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooksZoneApplication.class, args);
+	}
+
+}
