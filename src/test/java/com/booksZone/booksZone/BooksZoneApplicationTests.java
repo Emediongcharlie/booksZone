39c500d2808ecc72e30527a02f297b349daf7762
Emediong <emediongcharlie@gmail.com>
2025-06-06 09:09:50 +0100

    Week One Task

---
+++ src/test/java/com/booksZone/booksZone/BooksZoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.booksZone.booksZone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksZoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
